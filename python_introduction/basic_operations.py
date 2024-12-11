2cd3c5a4d9d72e941eb6d3b5be4623f11802a728
kirubel-Nigussie <kiranigussie1738@gmail.com>
2024-12-11 14:05:26 +0300

    commit basic_operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+Addition = number1 + number2
+Subtraction = number1 - number2
+Multiplication = number1 * number2
+print("Addition of " + str(number1) + " and " + str(number2) + " is " + str(Addition))
+print("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(Subtraction))
+print("Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(Multiplication))
