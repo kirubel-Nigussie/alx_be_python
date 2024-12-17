ec72166920231a6407f96ab180e181c72faae5ff
kirubel-Nigussie <kiranigussie1738@gmail.com>
2024-12-17 20:44:51 +0300

    new commit

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,7 @@
+positive_integer = int(input("Enter the size of the pattern:"))
+
+while positive_integer>0:
+    for num in positive_integer:
+        print("*", end="")
+    positive_integer=-1
+    print()
