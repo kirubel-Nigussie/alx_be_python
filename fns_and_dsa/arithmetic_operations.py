f1248c2ca7c5320ddab463d6232f948031c34659
kirubel-Nigussie <kiranigussie1738@gmail.com>
2025-01-07 22:12:30 +0300

    4th commit

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,14 @@
-def perform_operation (num1:float,num2:float,operation ):
- if (operation == "add"):
-  return num1 + num2
- elif(operation == "subtract"):
-  return num1 - num2
- elif(operation == "divide"):
-      if(num2 == 0):
-        print("give me a non zero number")
-      else:
-       return num1 / num2
- elif(operation == "multiply"):
-  return num1 * num2
+def perform_operation(num1: float, num2: float, operation):
+    if (operation == "add"):
+        return num1 + num2
+    elif (operation == "subtract"):
+        return num1 - num2
+    elif (operation == "divide"):
+        if (num2 == 0):
+            print("give me a non zero number")
+        else:
+            return num1 / num2
+    elif (operation == "multiply"):
+        return num1 * num2
 
  
