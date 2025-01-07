170f70aeb5b7af5e79fdf213fe3584df7247f217
kirubel-Nigussie <kiranigussie1738@gmail.com>
2025-01-07 22:26:10 +0300

    8th commit

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -5,7 +5,7 @@ def perform_operation(num1, num2, operation):
         return num1 - num2
     elif (operation == "multiply"):
         return num1 * num2
-    elif(num2 == 0):
+    elif num2 == 0:
         return "give me a non zero number"
 
     elif (operation == "divide"):
