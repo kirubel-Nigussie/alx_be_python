64a3bb18805555a65aa33c0c882effe6c2d08465
kirubel-Nigussie <kiranigussie1738@gmail.com>
2025-01-14 20:06:41 +0300

    1st commit

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,14 @@
+class BankAccount:
+    def __init__(self , account_balance) :
+        self.account_balance = account_balance
+        self.account_balance = 0
+def deposit(self, amount):
+    return amount + self.account_balance 
+def withdraw(self,amount):
+    if (amount< self.account_balance):
+        amount- self.account_balance
+        return "True"
+    else:
+        return "False"
+def display_balance(self):
+    print (self.account_balance)
