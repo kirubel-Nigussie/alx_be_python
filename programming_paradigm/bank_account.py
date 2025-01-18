84074a67ef99d3b5ee12ac57a1ba1231ebcad8f6
kirubel-Nigussie <kiranigussie1738@gmail.com>
2025-01-18 19:32:24 +0300

    11th commit

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -6,10 +6,9 @@ class BankAccount:
         self.account_balance += amount 
         return f"Deposited: ${self.account_balance}" 
     def withdraw(self, amount):
-        if (amount< self.account_balance):
-            self.account_balance -= amount 
-            return f"Withdrew: ${self.account_balance}"
-        else:
-            return "Insufficient funds"
+      if amount > 0 and self.account_balance >= amount:
+            self.account_balance -= amount
+            return True
+      return False
     def display_balance(self):
         print (f"Current Balance: ${self.account_balance:.2f}")
