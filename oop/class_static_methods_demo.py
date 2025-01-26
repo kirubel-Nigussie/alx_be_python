6b977fee35a1fec3982e06e420dc61e3bd6cf995
kirubel-Nigussie <kiranigussie1738@gmail.com>
2025-01-26 20:40:06 +0300

    9th commit

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -1,13 +1,15 @@
+
+
 class Calculator:
-    def __init__(self):
-         pass
+    
     calculation_type = "Arithmetic Operations"
 
-@staticmethod
-def add(a, b):
-    return f"The sum is: {a + b}" 
+    @staticmethod
+    def add(a, b):
+        
+        return a + b
 
-@classmethod
-def multiply(cls, a, b):
+    @classmethod
+    def multiply(cls, a, b):
         print(f"Calculation type: {cls.calculation_type}")
         return a * b
